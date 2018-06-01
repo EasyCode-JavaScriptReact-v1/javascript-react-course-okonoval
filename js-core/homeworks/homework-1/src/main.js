db57ab8cc5b238d77bb6acf0184a37dcc3878faa
Oleg Konoval <konoval09@gmail.com>
2018-06-01 13:33:32 +0300

    homework-1

---
+++ js-core/homeworks/homework-1/src/main.js
@@ -0,0 +1,20 @@
+const phoneBook = {
+    name: 'Oleg',
+    lastName: 'Konoval ',
+    phones:[
+        '0669778121',
+        '0932523243'
+    ],
+    messengers:{
+        viber: '0669778121',
+        telegram: '@okonoval'
+    },
+    address:{
+        street:'Washington',
+        buldingNum: '19',
+        aptNum: 'null'
+    },
+    isPremium: false
+};
+
+console.log(phoneBook);
